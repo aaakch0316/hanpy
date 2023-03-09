b17b4c6d2385bd796575c073cb7082af5a8a1d92
aaakch0316@gmail.com <aaakch0316@gmail.com>
2023-03-10 00:44:25 +0900

    _document.tsx [*] <Head> 태그 위치 이동

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,3 @@
-import Head from "next/head";
-import Image from "next/image";
 import { Inter } from "next/font/google";
 import styles from "@/styles/Home.module.css";
 import { useEffect, useState } from "react";
@@ -36,10 +34,6 @@ export default function Home() {
   }
   return (
     <>
-      <Head>
-        <title>Welcome to Black-Hole </title>
-        <link rel="shortcut icon" href="/favicon.png" />
-      </Head>
       <figure className={styles.figure}>
         {video ? video : null}
         <article className={styles.txt} onClick={handleClick}>
